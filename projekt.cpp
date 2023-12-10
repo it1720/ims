b3e6b00360a653b8869eaac67b4b11fab7f72861
David Dumbrovsky <dumbr@seznam.cz>
2023-12-10 21:08:11 +0100

    nejede

--- projekt.cpp
+++ projekt.cpp
@@ -2,12 +2,18 @@
 #include "simlib.h"
 using namespace std;
 
+int dostaljsemse = 0;
+int pocetZamestnancu = 0;
 int pocet_varek = 0;
 int pocet_kremu = 0;
 int sestavene = 0;
 int kontrola = 0;
 int zabaleno = 0;
+int palet = 0;
+int zabaliZamestnanci = 0;
+int krabicNaPalete = 0;
 bool funguje = true;
+bool jeVPoruse = false;
 Facility susenky_linka("Linka na susenky");
 Facility krem_linka("Linka na krem");
 
@@ -28,7 +34,7 @@ class susenky : public Process
 		Wait(30);
 		Release(susenky_linka);
 		pocet_varek++;
-		cout << "Varek: " << pocet_varek << endl;
+		// cout << "Varek: " << pocet_varek << endl;
 	}
 };
 
@@ -45,11 +51,11 @@ class krem : public Process
 		Wait(20);
 		Release(krem_linka);
 		pocet_kremu++;
-		cout << "Kremu: " << pocet_kremu << endl;
+		// cout << "Kremu: " << pocet_kremu << endl;
 	}
 };
 
-class kontrola_kvality : public Process
+class kontrolaKvality : public Process
 {
 	void Behavior()
 	{
@@ -60,7 +66,7 @@ class kontrola_kvality : public Process
 				Wait(15);
 				sestavene--;
 				kontrola++;
-				cout << "Kontrola kvality" << endl;
+				// cout << "Kontrola kvality" << endl;
 			}
 			else
 			{
@@ -81,14 +87,12 @@ class baleni : public Process
 				Wait(25);
 				kontrola--;
 				zabaleno++;
-				cout << "Zabaleno: " << zabaleno << endl;
 			}
 			else
 			{
 				Wait(1);
 			}
 		}
-
 	}
 };
 
@@ -102,12 +106,12 @@ class sestaveni : public Process
 			{
 				(new susenky)->Activate();
 				(new krem)->Activate();
-				cout << "Sestaveni susenky" << endl;
+				// cout << "Sestaveni susenky" << endl;
 				Wait(15);
 				pocet_varek--;
 				pocet_kremu--;
 				sestavene++;
-				cout << "Sestaveno: " << sestavene << endl;
+				// cout << "Sestaveno: " << sestavene << endl;
 			}
 			else
 			{
@@ -117,23 +121,116 @@ class sestaveni : public Process
 	}
 };
 
+class baleniDoKrabice : public Process
+{
+	void Behavior()
+	{
+		while (true)
+		{
+			if (jeVPoruse && zabaleno > 0)
+			{
+				zabaleno--;
+				zabaliZamestnanci += 100;
+			}
+			else if (zabaleno > 0)
+			{
+				Wait(30);
+				zabaleno--;
+				palet++;
+			}
+			else
+			{
+				Wait(1);
+			}
+		}
+	}
+};
+
+class zamestnanciBali : public Process
+{
+	void Behavior()
+	{
+		double tmptime = Uniform(7.5, 12.5);
+		Wait(tmptime);
+		// zabaliZamestnanci--;
+		krabicNaPalete++;
+		pocetZamestnancu++;
+	}
+};
+
+class zamestnanciLinka : public Process
+{
+	void Behavior()
+	{
+		while (true)
+		{
+			if (zabaliZamestnanci > 0 && pocetZamestnancu > 0)
+			{
+				// cout<<"Zabali zamestnanci: "<<zabaliZamestnanci<<endl;
+				(new zamestnanciBali)->Activate();
+				pocetZamestnancu--;
+			}
+			else
+			{
+				Wait(1);
+			}
+		}
+	}
+};
+
+class oprava : public Process
+{
+	void Behavior()
+	{
+		Wait(Uniform(20, 40));
+		jeVPoruse = false;
+	}
+};
+
 class porucha : public Process
 {
 	void Behavior()
 	{
-		Exp(60);
-		funguje = false;
+		while (true)
+		{
+			if (!jeVPoruse)
+			{
+				Wait(Uniform(40, 80));
+				(new oprava)->Activate();
+				// cout << "Porucha" << endl;
+				jeVPoruse = true;
+			}
+			else
+			{
+				Wait(1);
+			}
+		}
 	}
 };
 
 int main()
 {
-	Init(0, 550);
+	pocetZamestnancu = 500;
+	Init(0, 500);
 	(new susenky)->Activate();
 	(new krem)->Activate();
 	(new sestaveni)->Activate();
-	(new kontrola_kvality)->Activate();
+	(new kontrolaKvality)->Activate();
 	(new baleni)->Activate();
+	(new baleniDoKrabice)->Activate();
+	(new zamestnanciLinka)->Activate();
+	(new porucha)->Activate();
 	Run();
+	cout << "Zabalil stroj: " << palet << endl;
+	palet += krabicNaPalete/100;
+	cout << "Zabalil zamestnanci: " << palet << endl;
+	cout << "Varek: " << pocet_varek << endl;
+	cout << "Kremu: " << pocet_kremu << endl;
+	cout << "Sestaveno: " << sestavene << endl;
+	cout << "Kontrola: " << kontrola << endl;
+	cout << "Zabaleno: " << zabaleno << endl;
+	cout << "Palet: " << palet << endl;
+	cout << "Krabic na palete: " << krabicNaPalete << endl;
+	cout << dostaljsemse << endl;
 	return 0;
 }
