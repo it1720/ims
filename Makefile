84d723be3cc1148349c5194e318e6220d2aad56a
David Dumbrovsky <dumbr@seznam.cz>
2023-12-10 19:33:07 +0100

    pridani zakladnich trid

--- Makefile
+++ Makefile
@@ -1,4 +1,7 @@
 all:
 	g++ -Wall -pedantic -std=c++11 projekt.cpp -o projekt -lsimlib -lm
 run:
-	./ims
+	./projekt
+# Cíl pro vyčištění projektu (smazání vytvořených souborů)
+clean:
+	rm -f *.o projekt
--- projekt.cpp
+++ projekt.cpp
@@ -2,37 +2,81 @@
 #include "simlib.h"
 using namespace std;
 
-
 int pocet_varek = 0;
+int pocet_kremu = 0;
+int sestavene = 0;
 Facility susenky_linka("Linka na susenky");
+Facility krem_linka("Linka na krem");
 
 class susenky : public Process
 {
 	void Behavior()
 	{
 		Seize(susenky_linka);
-		cout << "Priprava surovin testa" << endl;
+		// cout << "Priprava surovin testa" << endl;
 		Wait(10);
-		cout << "Michani testa" << endl;
+		// cout << "Michani testa" << endl;
 		Wait(15);
-		cout << "Rozvalovani testa" << endl;
+		// cout << "Rozvalovani testa" << endl;
 		Wait(30);
-		cout << "Peceni" << endl;
+		// cout << "Peceni" << endl;
 		Wait(15);
-		cout << "Chlazeni" << endl;
+		// cout << "Chlazeni" << endl;
 		Wait(30);
 		Release(susenky_linka);
 		pocet_varek++;
-		cout << "Varka c." << pocet_varek << " je hotova" << endl;
-		(new susenky)->Activate();	
+		cout << "Varek: " << pocet_varek << endl;
 	}
 };
 
+class krem : public Process
+{
+	void Behavior()
+	{
+		Seize(krem_linka);
+		// cout << "Priprava surovin kremu" << endl;
+		Wait(10);
+		// cout << "Michani kremu" << endl;
+		Wait(20);
+		// cout << "Ztuhnuti kremu v lednici" << endl;
+		Wait(20);
+		Release(krem_linka);
+		pocet_kremu++;
+		cout << "Kremu: " << pocet_kremu << endl;
+	}
+};
+
+class sestaveni : public Process
+{
+	void Behavior()
+	{
+		while (true)
+		{
+			if (pocet_varek > 0 && pocet_kremu > 0)
+			{
+				(new susenky)->Activate();
+				(new krem)->Activate();
+				cout << "Sestaveni susenky" << endl;
+				Wait(15);
+				pocet_varek--;
+				pocet_kremu--;
+				sestavene++;
+				cout << "Sestaveno: " << sestavene << endl;
+			}
+			else
+			{
+				Wait(1);
+			}
+		}
+	}
+};
 
 int main()
 {
-	Init(0, 500);
+	Init(0, 550);
 	(new susenky)->Activate();
+	(new krem)->Activate();
+	(new sestaveni)->Activate();
 	Run();
 	return 0;
 }
